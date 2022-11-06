cd0c4abad69c57c68d176ba8b7c4df657c485b5c
Marc Reichel <marc.reichel@artemeon.de>
2022-11-06 11:57:41 +0100

    📦️ Update compiled files

--- dist/alpine-timeago.js
+++ dist/alpine-timeago.js
@@ -1905,7 +1905,9 @@
                   if (modifiers.includes('strict')) {
                       var unit = void 0;
                       if (modifiers.includes('unit')) {
-                          var innerUnit = modifiers[modifiers.findIndex(function (modifier) { return modifier === 'unit'; }) + 1] || undefined;
+                          var innerUnit = modifiers[modifiers.findIndex(function (modifier) {
+                              return modifier === 'unit';
+                          }) + 1] || undefined;
                           switch (innerUnit) {
                               case 'second':
                                   unit = 'second';
@@ -1932,7 +1934,9 @@
                       }
                       var roundingMethod = void 0;
                       if (modifiers.includes('rounding')) {
-                          var innerRounding = modifiers[modifiers.findIndex(function (modifier) { return modifier === 'rounding'; }) + 1] || undefined;
+                          var innerRounding = modifiers[modifiers.findIndex(function (modifier) {
+                              return modifier === 'rounding';
+                          }) + 1] || undefined;
                           switch (innerRounding) {
                               case 'floor':
                                   roundingMethod = 'floor';
@@ -1992,27 +1996,25 @@
           });
           cleanup(function () { return clearInterval(interval); });
       });
-      Alpine.magic('timeago', function () { return function (expression, pure, seconds, strictOptions) {
-          if (pure == null) {
-              pure = false;
-          }
-          if (seconds == null) {
-              seconds = false;
-          }
-          if (strictOptions != null && (strictOptions['strict'] || undefined)) {
-              return formatDistanceToNowStrict(expression, {
+      Alpine.magic('timeago', function () {
+          return function (expression, pure, seconds, strictOptions) {
+              if (pure === void 0) { pure = false; }
+              if (seconds === void 0) { seconds = false; }
+              if (strictOptions && strictOptions.strict) {
+                  return formatDistanceToNowStrict(expression, {
+                      addSuffix: !pure,
+                      unit: strictOptions.unit,
+                      roundingMethod: strictOptions.roundingMethod,
+                      locale: locale
+                  });
+              }
+              return formatDistanceToNow(expression, {
                   addSuffix: !pure,
-                  unit: strictOptions['unit'] || undefined,
-                  roundingMethod: strictOptions['roundingMethod'] || undefined,
+                  includeSeconds: seconds,
                   locale: locale
               });
-          }
-          return formatDistanceToNow(expression, {
-              addSuffix: !pure,
-              includeSeconds: seconds,
-              locale: locale
-          });
-      }; });
+          };
+      });
   }
   TimeAgo.configure = function (config) {
       if (config.locale) {
